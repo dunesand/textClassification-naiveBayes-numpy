c8daf4a84a63a21a581568a29608f1cb807c09f1
dunesand <45661111+dunesand@users.noreply.github.com>
2020-07-07 18:29:18 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> #准备数据：从文本中构建词向量
+ #准备数据：从文本中构建词向量
 def loadDataSet():
     postingList=[['my','dog','has','flea','problems','help','please'],\
                  ['maybe','not','take','him','to','dog','park','stupid'],\
